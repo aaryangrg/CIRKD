1c80c59a8b55a0013e7b6296d50d60505cc12bae
Aaryan Garg <aaryan.garg1@gmail.com>
2023-10-27 15:01:50 +0530

    ade20k image resizing

--- dataset/ade20k.py
+++ dataset/ade20k.py
@@ -149,7 +149,7 @@ class ADEDataValSet(data.Dataset):
         image = cv2.imread(datafiles["img"], cv2.IMREAD_COLOR)
         label = cv2.imread(datafiles["label"], cv2.IMREAD_GRAYSCALE)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        
+
         size = image.shape
         
         name = osp.splitext(osp.basename(datafiles["img"]))[0]
@@ -162,8 +162,27 @@ class ADEDataValSet(data.Dataset):
         std = np.array([0.229, 0.224, 0.225])
         image -= mean
         image /= std
+        
+        #RESIZING
+        img_h, img_w = label.shape
+        pad_h = max(self.crop_h - img_h, 0)
+        pad_w = max(self.crop_w - img_w, 0)
+        if pad_h > 0 or pad_w > 0:
+            img_pad = cv2.copyMakeBorder(image, 0, pad_h, 0, 
+                pad_w, cv2.BORDER_CONSTANT, 
+                value=(0.0, 0.0, 0.0))
+            label_pad = cv2.copyMakeBorder(label, 0, pad_h, 0, 
+                pad_w, cv2.BORDER_CONSTANT,
+                value=(self.ignore_label,))
+        else:
+            img_pad, label_pad = image, label
+        img_h, img_w = label_pad.shape
+        h_off = random.randint(0, img_h - self.crop_h)
+        w_off = random.randint(0, img_w - self.crop_w)
+        image = np.asarray(img_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
+        label = np.asarray(label_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
         image = image.transpose((2, 0, 1))
-        image = np.asarray(image, np.float32)
+        # image = np.asarray(image, np.float32)
         label = np.asarray(label, np.float32)
         label = label - 1
 
